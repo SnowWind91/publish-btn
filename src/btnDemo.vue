1ab4347f3affcdff4345b8c5365084ad0f84e417
zuoxu <zuoxu91@qq.com>
2021-04-25 15:01:05 +0800

    first commit

---
+++ src/btnDemo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="btn">
+    <button @click="handle">{{text}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'btn', // 组件的name属性（后面有提到这里有个坑）
+  props: {
+    text: { // 文本
+      type: String,
+      default () {
+        return ''
+      }
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods:{
+    handle(){
+      alert('我点击了')
+    }
+  }
+}
+</script>
